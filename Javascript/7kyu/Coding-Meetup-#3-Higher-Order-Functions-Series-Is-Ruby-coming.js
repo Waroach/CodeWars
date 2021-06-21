19031324aaebf8b9b1365b53b10dfc878a17e920
waroach <brent.rankin@gmail.com>
2021-06-21 10:41:03 -0700

    Completed Kata Coding-Meetup-3-isRubyComing

---
+++ Javascript/7kyu/Coding-Meetup-#3-Higher-Order-Functions-Series-Is-Ruby-coming.js
@@ -0,0 +1,22 @@
+// You will be given an array of objects
+// (associative arrays in PHP) representing
+// data about developers who have signed up
+// to attend the next coding meetup that you are organising.
+// Your task is to return:
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+
+function isRubyComing(list) {
+    for (i=0; i<list.length;i++){
+        if(list[i].language === 'Ruby'){
+            return true
+        }
+    }
+    return false
+}
+
+isRubyComing([
+    { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+    { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+])
