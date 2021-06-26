077b12eb74eeb04ff9fc42230691f231b8b32e1b
waroach <brent.rankin@gmail.com>
2021-06-26 10:15:42 -0700

    Coding-meetup-11-getAverageAge

---
+++ Javascript/7kyu/Coding-Meetup-#11-Higher-Order-Functions-Series-Find-the-average-age.js
@@ -0,0 +1,19 @@
+// You will be given a sequence of objects representing data about developers
+//  who have signed up to attend the next coding meetup that you are organising.
+// write a function that returns the average age of developers (rounded to the
+// nearest integer). In the example above your function should return 50 (number).
+// Notes:
+// The input array will always be valid and formatted as in the example above.
+// Age is represented by a number which can be any positive integer.
+
+
+function getAverageAge(list) {
+  // thank you for checking out the Coding Meetup kata :)
+}
+
+
+// Given the following input array:
+console.log(getAverageAge([
+{ firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+{ firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+]), "test")
