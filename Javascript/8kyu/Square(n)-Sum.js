5997cac53be60e9ba1d6d481a2e5c685658c933f
waroach <brent.rankin@gmail.com>
2021-06-28 09:29:12 -0700

    Completed Kata squareSum

--- Javascript/8kyu/Square(n)-Sum.js
+++ Javascript/8kyu/Square(n)-Sum.js
@@ -1,11 +1,9 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-
 function squareSum(numbers){
-
+    return numbers.reduce((total,number)=>Math.pow(number,2)+total,0)
 }
-
 
 console.log(squareSum([1,2]), 5)
 console.log(squareSum([0, 3, 4, 5]), 50)
