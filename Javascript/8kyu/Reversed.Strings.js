72aee15ed3a4dda45971d52a635532f5fe5cb4b7
waroach <brent.rankin@gmail.com>
2021-06-28 18:24:54 -0700

    Completed Kata reversed-Strings

--- Javascript/8kyu/Reversed.Strings.js
+++ Javascript/8kyu/Reversed.Strings.js
@@ -2,10 +2,19 @@
 // 'world'  =>  'dlrow'
 
 function solution(str){
-    return str.split('').reverse().join('')
+    var newString = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 }
+                            // First Try
+// function solution(str){
+//     return str.split('').reverse().join('')
+// }
 
 console.log(solution('world'), 'dlrow')
 console.log(solution('hello'), 'olleh')
 console.log(solution(''), '')
 console.log(solution('h'), 'h')
+console.log(solution('Greetings from Earth'), 'htraE morf sgniteerG')
