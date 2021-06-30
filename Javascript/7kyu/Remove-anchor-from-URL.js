e48f41e7f035ed751896e3ecb7e087d94f2270fa
waroach <brent.rankin@gmail.com>
2021-06-30 09:18:25 -0700

    Started Kata removeUrlAnchor

---
+++ Javascript/7kyu/Remove-anchor-from-URL.js
@@ -0,0 +1,8 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+function removeUrlAnchor(url){
+    // TODO: complete
+}
+
+console.log(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com')
+console.log(removeUrlAnchor('www.codewars.com?page=1') , 'www.codewars.com?page=1')
