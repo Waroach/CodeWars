b323f404d348a51a7214b9a24d6565ed0b6863ea
waroach <brent.rankin@gmail.com>
2021-07-03 01:54:46 -0700

    Completed Kata Training-JS-#9

--- Javascript/8kyu/Training-JS/Training-JS-#9-loop-statement--while-and-do-while.js
+++ Javascript/8kyu/Training-JS/Training-JS-#9-loop-statement--while-and-do-while.js
@@ -1,5 +1,13 @@
 function padIt(str,n){
-    //coding here
+    while(n>0){
+        if(n % 2 === 0){
+            str = str+'*'
+        }else{
+            str = '*'+str
+        }
+        n--
+    }
+    return str
 }
 
 // Task
