ff2f2ac0560ae1e9044723ec2a5638d5e0861289
waroach <brent.rankin@gmail.com>
2021-07-03 01:41:25 -0700

    Started Kata Training-JS-#9

---
+++ Javascript/8kyu/Training-JS/Training-JS-#9-loop-statement--while-and-do-while.js
@@ -0,0 +1,17 @@
+function padIt(str,n){
+    //coding here
+}
+
+// Task
+// Coding in function padIt, function accept 2 parameters:
+// 1) str, it's a string representing the string to pad,
+// we need pad some "*" at leftside or rightside of str
+// 2) n, it's a number, how many times to pad the string.
+// Behaviour
+// You need to write a loop statement within the function
+// that loops n times. Each time through the loop it will
+// add one * to str, alternating on which side it is
+// padded: the first time will add a * to the left side
+// of str, the second time will add a * to the right
+// side, and so on.
+// Finally, return the padded string.
