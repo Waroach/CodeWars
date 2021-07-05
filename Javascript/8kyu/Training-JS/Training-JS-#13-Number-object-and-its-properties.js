298f9233e42d32c7094cfd26de58736322546995
waroach <brent.rankin@gmail.com>
2021-07-05 11:15:57 -0700

    Completed Kata Training-JS-#13

--- Javascript/8kyu/Training-JS/Training-JS-#13-Number-object-and-its-properties.js
+++ Javascript/8kyu/Training-JS/Training-JS-#13-Number-object-and-its-properties.js
@@ -1,6 +1,31 @@
 function whatNumberIsIt(n){
-    //coding here
-    
+    if(isNaN(n)){
+        return "Input number is Number.NaN"
+    }else{
+        switch (n) {
+            case 1/0:
+                return "Input number is Number.POSITIVE_INFINITY"
+                break;
+            case 100:
+                return "Input number is 100"
+                break;
+            case 1.7976931348623157e+308:
+                return "Input number is Number.MAX_VALUE"
+                break;
+            case 5e-324:
+                return "Input number is Number.MIN_VALUE"
+                break;
+            case -Number.MAX_VALUE*2:
+                return "Input number is Number.NEGATIVE_INFINITY"
+                break;
+            case Infinity+1:
+                return "Input number is Number.POSITIVE_INFINITY"
+                break;
+            default :
+                return `Input number is ${n}`
+                break;
+        }
+    }
 }
 
 // Task
