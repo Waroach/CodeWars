88fbc3e98f441a9bd4e582e02188cea1a59f8458
waroach <brent.rankin@gmail.com>
2021-07-07 11:39:50 -0700

    Completed Kata Training-JS-#17

--- Javascript/8kyu/Training-JS/Training-JS-#17-Methods-of-String-object--indexOf--lastIndexOf.and.search.js
+++ Javascript/8kyu/Training-JS/Training-JS-#17-Methods-of-String-object--indexOf--lastIndexOf.and.search.js
@@ -1,9 +1,16 @@
 function firstToLast(str,c){
-    //coding here..
-    
+    let first = str.indexOf(c)
+    let last = str.lastIndexOf(c)
+    if(first === -1 && last === -1){ return -1 }
+    if(first >= 0 && last >= 0){ return last - first }
 }
 
 /*
+indexOf() retrieves a string from the location of the fromindex to the right
+    (If fromindex is omitted, began retrieval from index0).
+lastIndexOf() retrieves a string from the location of the fromindex to the left
+    (If fromindex is omitted, began retrieval from last chars).
+
 Task
 Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
 Please return the gap between the first position of c and the last position of c.
