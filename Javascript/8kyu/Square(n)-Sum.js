d6e3878c2a01484e5a518ae1ed1c30064a0ef4c1
waroach <brent.rankin@gmail.com>
2021-07-12 11:57:38 -0700

    Cleaned up on Kata Square n-Sum

--- Javascript/8kyu/Square(n)-Sum.js
+++ Javascript/8kyu/Square(n)-Sum.js
@@ -1,7 +1,7 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
-const squareSum = numbers => numbers.reduce((a,c) => a + Math.pow(c, 2))
+const squareSum = numbers => numbers.reduce((a,c) => a + (c*c), 0)
 
 // function squareSum(numbers){
 //     return numbers.reduce((total,number)=>Math.pow(number,2)+total,0)
