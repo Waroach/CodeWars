6398f764a42f41a005412fc74b520fdb436f84f5
waroach <brent.rankin@gmail.com>
2021-07-14 12:22:35 -0700

    Completed Kata Remove-Consecutive-Duplicates

--- Javascript/7kyu/Remove-consecutive-duplicate-words.js
+++ Javascript/7kyu/Remove-consecutive-duplicate-words.js
@@ -1,5 +1,7 @@
-const removeConsecutiveDuplicates = s => // your perfect code...
+const removeConsecutiveDuplicates = s => s.split(' ').filter((w,i,a)=>w!==a[i+1]).join(' ')
 
+console.log(removeConsecutiveDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'), 'alpha beta gamma delta alpha beta gamma delta')
+console.log(removeConsecutiveDuplicates('alpha alpha beta beta alpha alpha'), "alpha beta alpha")
 
 /*
 Your task is to remove all consecutive duplicate words from a string,
