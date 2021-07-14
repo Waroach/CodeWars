c69fa244291b76974972e7697ed4ada65f4ebc02
waroach <brent.rankin@gmail.com>
2021-07-13 22:43:11 -0700

    Completed Kata Upper-Body-Strength

--- Javascript/7kyu/Upper--body--Strength.js
+++ Javascript/7kyu/Upper--body--Strength.js
@@ -1,5 +1,5 @@
-function alexMistakes(numberOfKata, timeLimit){
-    //TODO
+function alexMistakes(numberOfKata, timeLimit){  
+    return Math.floor(Math.log2((timeLimit - 6 * numberOfKata) / 5 + 1))
 }
 
 /*
