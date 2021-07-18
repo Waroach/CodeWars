f76311dedd5d557b2421bd50045b1adc9c2a202e
waroach <brent.rankin@gmail.com>
2021-07-18 07:57:30 -0700

    Cleaned Kata Break-camelCase

--- Javascript/6kyu/Break-camelCase.js
+++ Javascript/6kyu/Break-camelCase.js
@@ -1,6 +1,9 @@
-function solution(string) {
-    return string.split('').map(e => e === e.toUpperCase() ? " "+e:e).join('')
-}
+solution=string=>string.split('').map(e=>e===e.toUpperCase()?" "+e:e).join('')
+
+                            // Frist try works great
+// function solution(string) {
+//     return string.split('').map(e => e === e.toUpperCase() ? " "+e:e).join('')
+// }
 
 /*
 Complete the solution so that the function will break up camel casing, using a space between words.
