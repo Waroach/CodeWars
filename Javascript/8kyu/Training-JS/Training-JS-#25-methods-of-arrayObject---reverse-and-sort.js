b8ce6d467349854c0be43371ffb237d0c881d9d4
waroach <brent.rankin@gmail.com>
2021-07-22 14:50:20 -0700

    Completed Kata Training-JS-25

--- Javascript/8kyu/Training-JS/Training-JS-#25-methods-of-arrayObject---reverse-and-sort.js
+++ Javascript/8kyu/Training-JS/Training-JS-#25-methods-of-arrayObject---reverse-and-sort.js
@@ -1,6 +1,11 @@
 function sortIt(arr){
-    //coding here...
-    
+    return arr.slice().sort((a,b)=>{
+        let countA = arr.filter(v=>v===a).length
+        let countB = arr.filter(v=>v===b).length
+        if(countA === countB){return b-a}
+        else if(countA>countB){return 1}
+        else{return -1}
+    })
 }
 
 /*
