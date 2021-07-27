b2448995c68b5d1d847c91cbb2105c151c4b9967
waroach <brent.rankin@gmail.com>
2021-07-27 16:05:52 -0700

    Redo of 8kyu Reversed-Words

--- Javascript/8kyu/Reversed-Words.js
+++ Javascript/8kyu/Reversed-Words.js
@@ -1,7 +1,10 @@
 // Complete the solution so that it reverses all of the words within the string passed in.
+                            // First Try
+// function reverseWords(str){
+//     return str.split(' ').reverse().join(' ')
+// }
 
-function reverseWords(str){
-    return str.split(' ').reverse().join(' ')
-}
+                            // Second Try
+reverseWords=s=>s.split(' ').reverse().join(' ')
 
 console.log(reverseWords("The greatest victory is that which requires no battle"), "battle no requires which that is victory greatest The")
