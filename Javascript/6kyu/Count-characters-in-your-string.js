d29104ce9139e7d99aec70bf40a8c92ae92a6848
waroach <brent.rankin@gmail.com>
2021-07-26 17:29:30 -0700

    Need to work more on this kata

--- Javascript/6kyu/Count-characters-in-your-string.js
+++ Javascript/6kyu/Count-characters-in-your-string.js
@@ -5,6 +5,8 @@ function count(s){
 /*
 https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
 
+I need to REDO this one
+
 The main idea is to count all the occurring characters in a string.
 If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 What if the string is empty? Then the result should be empty object literal, {}.
