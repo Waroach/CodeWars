44b7560a609af567edabb3479ffd7c57cb4d9e3a
waroach <brent.rankin@gmail.com>
2021-07-27 15:58:20 -0700

    Redo of 8kyu Reversed-String

--- Javascript/8kyu/Reversed.Strings.js
+++ Javascript/8kyu/Reversed.Strings.js
@@ -1,17 +1,21 @@
 // Complete the solution so that it reverses the string passed into it.
 // 'world'  =>  'dlrow'
 
-function solution(str){
-    var newString = "";
-    for (i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
-}
                             // First Try
+// function solution(str){
+//     var newString = "";
+//     for (i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//     }
+//     return newString;
+// }
+                            // Second Try
 // function solution(str){
 //     return str.split('').reverse().join('')
 // }
+
+                            // Cleaned up Version
+solution=s=>s.split('').reverse().join('')
 
 console.log(solution('world'), 'dlrow')
 console.log(solution('hello'), 'olleh')
