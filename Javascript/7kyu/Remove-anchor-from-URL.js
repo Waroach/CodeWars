0859f33a560c9223116fb84a0116190e6deef7c8
waroach <brent.rankin@gmail.com>
2021-07-27 16:14:03 -0700

    Redo of 7kyu Remove-anchor-from-URL

--- Javascript/7kyu/Remove-anchor-from-URL.js
+++ Javascript/7kyu/Remove-anchor-from-URL.js
@@ -1,8 +1,11 @@
 // Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+                            // First Try
+// function removeUrlAnchor(url){
+//     return url.split('#')[0]
+// }
 
-function removeUrlAnchor(url){
-    return url.split('#')[0]
-}
+                            // Cleaned up
+removeUrlAnchor=url=>url.split('#')[0]
 
 console.log(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com')
 console.log(removeUrlAnchor('www.codewars.com?page=1') , 'www.codewars.com?page=1')
