178503a47b9c4b97f82970f051371e1e4245e06b
waroach <brent.rankin@gmail.com>
2021-07-28 10:00:21 -0700

    Added notes on 7kyu Javascript-filter-1

--- Javascript/7kyu/Javascript-filter-1.js
+++ Javascript/7kyu/Javascript-filter-1.js
@@ -6,7 +6,9 @@ function searchNames(logins){
 https://www.codewars.com/kata/525d9b1a037b7a9da7000905/train/javascript
 
 Need to redo and work on this one
-Use l[0].endsWith('_')
+Try either of these options
+l[0].endsWith('_')
+.filter(el => el[0].match(/[_]$/))
 
 
 
