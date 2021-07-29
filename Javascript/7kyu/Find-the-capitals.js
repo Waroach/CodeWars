ced69b00a518ca31c2f625cdb8b3bf608c539f98
waroach <brent.rankin@gmail.com>
2021-07-29 14:33:19 -0700

    Added link to 7kyu Find-The-Capitols

--- Javascript/7kyu/Find-the-capitals.js
+++ Javascript/7kyu/Find-the-capitals.js
@@ -5,6 +5,12 @@ var capitals = function (word) {
 }
 
 /*
+https://www.codewars.com/kata/539ee3b6757843632d00026b/train/javascript
+
+Should redo this without the Turnery...
+
+
+
 Instructions
 Write a function that takes a single string (word) as argument.
 The function must return an ordered list containing the indexes of all capital letters in the string.
