d4ada85be17c66ca1b3d49d55f09ae24344af269
waroach <brent.rankin@gmail.com>
2021-07-29 18:36:35 -0700

    Worked on 6kyu Array-Combos

--- Javascript/6kyu/Array-combinations.js
+++ Javascript/6kyu/Array-combinations.js
@@ -1,9 +1,27 @@
-function solve(arr) {
-    return 0
-}
+function solve(arr) {\
+    let newArr = []
+    let total = 0
+      arr.map(a=>{
+        a.reduce(e=>)
+      })
+  }
+  // [[1,2],[4],[5,6]]
+  // 1,4,5
+  // 2,4,5
+  // 1,4,6
+  // 2,4,6
+  
+  // [1,2], 0 ,[[1,2],[4],[5,6]]
+  //  (Map, Reduce, Set)
+  console.log(solve([[1,2],[4],[5,6]]),4)
+  // console.log(solve([[1,2],[4,4],[5,6,6]]),4)
+  // console.log(solve([[1,2],[3,4],[5,6]]),8)
+  // console.log(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
 
 /*
 https://www.codewars.com/kata/59e66e48fc3c499ec5000103/train/javascript
+(Map, Reduce, Set)
+
 
 In this Kata, you will be given an array of arrays and your task will be to return
 the number of unique arrays that can be formed by picking exactly one element from each subarray.
