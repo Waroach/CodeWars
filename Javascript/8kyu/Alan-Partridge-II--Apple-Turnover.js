18683e7783f8e0a2ed9833d680de6900f784365f
waroach <brent.rankin@gmail.com>
2021-08-04 12:55:54 -0700

    Completed 8kyu Alan-Partridge-II-Apple-Turnover

--- Javascript/8kyu/Alan-Partridge-II--Apple-Turnover.js
+++ Javascript/8kyu/Alan-Partridge-II--Apple-Turnover.js
@@ -1,6 +1,10 @@
 function apple(x){
-
-}
+    if(Math.pow(x,2)>1000){
+        return `It's hotter than the sun!!`
+    }else{
+        return `Help yourself to a honeycomb Yorkie for the glovebox.`
+    }
+}'Help yourself to a honeycomb Yorkie for the glovebox.'
 
 /*
 https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
