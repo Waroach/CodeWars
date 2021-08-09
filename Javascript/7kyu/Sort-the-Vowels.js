5119b7340bd99b2a6e5a968d746be30043161f98
waroach <brent.rankin@gmail.com>
2021-08-09 16:24:11 -0700

    Started 7kyu Sort-the-Vowles

---
+++ Javascript/7kyu/Sort-the-Vowels.js
@@ -0,0 +1,25 @@
+function sortVowels(s){
+    //code
+}
+
+console.log(sortVowels('Codewars'), 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|')
+console.log(sortVowels('Rnd Te5T'), 'R|\nn|\nd|\n |\nT|\n|e\n5|\nT|')
+console.log(sortVowels(1337), '')
+console.log(sortVowels(undefined), '')
+
+/*
+https://www.codewars.com/kata/59e49b2afc3c494d5d00002a/train/javascript
+
+
+Sort the Vowels!
+In this kata, we want to sort the vowels in a special format.
+Task
+Write a function which takes a input string s and return
+a string in the following way:
+Notes:
+List all the Vowels on the right side of |
+List every character except Vowels on the left side of |
+Return every character in its original case
+Each line is seperated with \n
+Invalid input ( undefined / null / integer ) should return an empty string
+*/
