e28c521fa5a915e2a29704855ced5fb615613365
waroach <brent.rankin@gmail.com>
2021-08-09 16:00:50 -0700

    Started 8kyu Hex-To-Decimal

---
+++ Javascript/8kyu/Hex-to-Decimal.js
@@ -0,0 +1,10 @@
+function hexToDec(hexString){
+    //your code here
+}
+
+/*
+https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+
+Complete the function which converts hex number 
+(given as a string) to a decimal number.
+*/
