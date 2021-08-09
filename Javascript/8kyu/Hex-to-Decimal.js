7ac51ce07bc07f68c5194cd43377900bec4307fe
waroach <brent.rankin@gmail.com>
2021-08-09 16:08:16 -0700

    Worked on 8kyu Hex-To-Decimal

--- Javascript/8kyu/Hex-to-Decimal.js
+++ Javascript/8kyu/Hex-to-Decimal.js
@@ -1,6 +1,12 @@
 function hexToDec(hexString){
-    //your code here
+    parseInt(hexString, 16)
 }
+
+console.log(hexToDec("1"), 1)
+console.log(hexToDec("a"), 10)
+console.log(hexToDec("10"), 16)
+console.log(hexToDec("FF"), 255)
+console.log(hexToDec("-C"), -12)
 
 /*
 https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
