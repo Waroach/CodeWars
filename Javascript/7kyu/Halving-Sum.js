d5b01f986202a52356d9c6036fac53f53c154055
waroach <brent.rankin@gmail.com>
2021-08-10 08:38:54 -0700

    Started 7kyu Halving-Sum

---
+++ Javascript/7kyu/Halving-Sum.js
@@ -0,0 +1,20 @@
+function halvingSum(n) {
+    for(i=2;n!=1;i+i){
+        
+    }
+}
+
+console.log(halvingSum(25),47)
+console.log(halvingSum(127),247)
+
+/*
+https://www.codewars.com/kata/5a58d46cfd56cb4e8600009d/train/javascript
+
+
+Task
+Given a positive integer n, calculate the following sum:
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+*/
