2847b2386120013bb22cd8ba20761d743e6d62e5
waroach <brent.rankin@gmail.com>
2021-08-11 08:24:33 -0700

    Pseudocoded the 6kyu Highest-Scoring-Word

--- Javascript/6kyu/Highest-Scoring-Word.js
+++ Javascript/6kyu/Highest-Scoring-Word.js
@@ -5,6 +5,18 @@ function high(x){
 console.log(high('what time are we climbing up the volcano'), 'volcano')
 console.log(high('aa b'), 'aa')
 
+/*
+Pseudocode
+1) First we need to get a indecies + 1 of the alphabet
+i.e. a,b,c = 1,2,3 not 0,1,2
+2) Get points for each letter
+3) Then sum those points
+4) hold the word (as a string) and its point value. (hString, hSum)
+5) compare to the next word (cString, cSum)
+6) if the next word sum is higher reassaign the current word to the High String and Sum
+7) if not go to the next word
+8) return the highest scoring word as a string.
+*/
 
 /*
 https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
