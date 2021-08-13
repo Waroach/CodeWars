08d611b7a28a9eda45e0d6ad38ccbc0e5a0a0970
waroach <brent.rankin@gmail.com>
2021-08-12 19:49:05 -0700

    Started 8kyu Remove-String-Spaces

---
+++ Javascript/8kyu/Remove-String-Spaces.js
@@ -0,0 +1,10 @@
+function noSpace(x){
+
+}
+
+/*
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+
+Simple, remove the spaces from the string, then return the resultant string.
+*/
