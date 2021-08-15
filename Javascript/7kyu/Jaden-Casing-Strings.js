d667babf0c097ec7b0f8193cc26f57438412595b
waroach <brent.rankin@gmail.com>
2021-08-15 14:10:59 -0700

    Completed 7kyu Jaden-Casing-Strings

--- Javascript/7kyu/Jaden-Casing-Strings.js
+++ Javascript/7kyu/Jaden-Casing-Strings.js
@@ -1,5 +1,9 @@
 String.prototype.toJadenCase = function () {
-    //...
+    String.prototype.toJadenCase = function () {
+        return this.split(" ").map(function(word){
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        }).join(" ")
+    }
 };
 
 console.log(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real")
