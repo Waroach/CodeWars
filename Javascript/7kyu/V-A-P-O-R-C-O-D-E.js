7b09162859eb88125015fa5c9afcb48901e4f579
waroach <brent.rankin@gmail.com>
2021-08-17 11:39:59 -0700

    Cleaned up 7kyu V-A-P-O-R-C-O-D-E

--- Javascript/7kyu/V-A-P-O-R-C-O-D-E.js
+++ Javascript/7kyu/V-A-P-O-R-C-O-D-E.js
@@ -1,6 +1,9 @@
-function vaporcode(string) {
-    return string.split(' ').join('').split('').map(l=>l=l.toUpperCase()).join("  ")
-}
+vaporcode=s=>s.split(' ').join('').split('').map(l=>l=l.toUpperCase()).join("  ")
+
+                            // First Try
+// function vaporcode(string) {
+//     return string.split(' ').join('').split('').map(l=>l=l.toUpperCase()).join("  ")
+// }
 
 console.log(vaporcode("Let's go to the movies"),"L  E  T  '  S  G  O  T  O  T  H  E  M  O  V  I  E  S")
 console.log(vaporcode("Why isn't my code working?"),"W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?")
