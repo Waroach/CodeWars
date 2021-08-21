c679d77f9b8ce7f58ff155c52e599377943625d9
waroach <brent.rankin@gmail.com>
2021-08-21 03:21:09 -0700

    Started 7kyu Filter-the-number

---
+++ Javascript/7kyu/Filter-the-number.js
@@ -0,0 +1,22 @@
+var FilterString = function(value) {
+    //Complete this function :)
+}
+
+console.log(FilterString("123"), 123, 'Just return the numbers')
+console.log(FilterString("a1b2c3"), 123, 'Just return the numbers')
+console.log(FilterString("aa1bb2cc3dd"), 123, 'Just return the numbers')
+
+/*
+https://www.codewars.com/kata/55b051fac50a3292a9000025/train/javascript
+
+
+Filter the number
+Oh no! The number has been mixed up with the text.
+Your goal is to retreive the number from the text,
+can you return the number back to it's original state?
+Task
+Your task is to return a number from a string.
+Details
+You will be given a string of numbers and letters mixed up,
+you have to return all the numbers in that string in the order they occur.
+*/
