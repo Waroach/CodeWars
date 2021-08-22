dabdd63fc04c5ad3967f7463df9c157f24f863fa
waroach <brent.rankin@gmail.com>
2021-08-22 01:18:15 -0700

    Added tests 6kyu Your-Order-Please

--- Javascript/6kyu/Your-Order-Please.js
+++ Javascript/6kyu/Your-Order-Please.js
@@ -10,8 +10,13 @@ function order(words){
     return worder.join(' ')
 }
 
+console.log(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+console.log(order(""), "", "empty input should return empty string" )
+
 /*
 https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript
+
 
 Your task is to sort a given string. Each word in the string will contain a single number.
 This number is the position the word should have in the result.
