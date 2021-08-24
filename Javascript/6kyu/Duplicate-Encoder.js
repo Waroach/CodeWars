f9b23ab4758edcd91c82ea37ebe4169460496767
waroach <brent.rankin@gmail.com>
2021-08-24 10:35:34 -0700

    Completed 6kyu Duplicate-Encoder

--- Javascript/6kyu/Duplicate-Encoder.js
+++ Javascript/6kyu/Duplicate-Encoder.js
@@ -1,5 +1,11 @@
 function duplicateEncode(word){
-    // ...
+    return word
+    .toLowerCase()
+    .split('')
+    .map( function (v, i, arr) {
+    return arr.indexOf(v) == arr.lastIndexOf(v) ? '(' : ')'
+    })
+    .join('');
 }
 
 
