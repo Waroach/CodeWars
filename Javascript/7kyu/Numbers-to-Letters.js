d782cfa61b26193336843acd7648f6643e882630
waroach <brent.rankin@gmail.com>
2021-08-27 14:43:49 -0700

    Fixed spelling errors

--- Javascript/7kyu/Numbers-to-Letters.js
+++ Javascript/7kyu/Numbers-to-Letters.js
@@ -2,9 +2,9 @@ function switcher(x){
 
 }
 
-consol.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars')
-consol.log(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw')
-consol.log(switcher(['4', '24']), 'wc')
+console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars')
+console.log(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw')
+console.log(switcher(['4', '24']), 'wc')
 
 /*
 https://www.codewars.com/kata/57ebaa8f7b45ef590c00000c/train/javascript
