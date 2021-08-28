66c92d6ca6c18e8d2084e4a361d37120ba69df59
waroach <brent.rankin@gmail.com>
2021-08-28 13:01:26 -0700

    Added some test cases 7kyu Sort-by-Last-Char

--- Javascript/7kyu/Sort-by-Last-Char.js
+++ Javascript/7kyu/Sort-by-Last-Char.js
@@ -3,9 +3,9 @@ function last(x){
 }
 
 
-Test.assertSimilar(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
-Test.assertSimilar(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
-Test.assertSimilar(last('take me to semynak'), ['take', 'me', 'semynak', 'to'])
+console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
+console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
+console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to'])
 
 /*
 https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/train/javascript
