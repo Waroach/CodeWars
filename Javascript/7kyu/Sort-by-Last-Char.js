69a0a8dbb5f2eee804fef69f100681ddd2ba7316
waroach <brent.rankin@gmail.com>
2021-08-28 12:59:10 -0700

    Started 7kyu Sort-by-Last-Char

---
+++ Javascript/7kyu/Sort-by-Last-Char.js
@@ -0,0 +1,18 @@
+function last(x){
+
+}
+
+
+Test.assertSimilar(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
+Test.assertSimilar(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
+Test.assertSimilar(last('take me to semynak'), ['take', 'me', 'semynak', 'to'])
+
+/*
+https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0/train/javascript
+
+
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+All inputs will be valid.
+*/
