ae11048c79d29f0f035a70328b1ec109138108f7
waroach <brent.rankin@gmail.com>
2021-08-29 16:26:00 -0700

    Completed 6kyu Sort-the-Odd

--- Javascript/6kyu/Sort-the-odd.js
+++ Javascript/6kyu/Sort-the-odd.js
@@ -1,5 +1,6 @@
-function sortArray(array) {
-    // Return a sorted array.
+function sortArray(arr) {
+    const oddNums = arr.filter((x) => x % 2).sort((a,b)=>a-b)
+    return arr.map(n => n % 2 ? oddNums.shift() : n)
 }
 
 console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
