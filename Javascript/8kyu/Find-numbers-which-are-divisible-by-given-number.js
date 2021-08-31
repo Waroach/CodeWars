9fc90bf733f804c60586d709399d4fc608f05a20
waroach <brent.rankin@gmail.com>
2021-08-31 10:47:11 -0700

    Started 8kyu Divisble-by-X

---
+++ Javascript/8kyu/Find-numbers-which-are-divisible-by-given-number.js
@@ -0,0 +1,22 @@
+function divisibleBy(numbers, divisor){
+
+}
+
+console.log(divisibleBy([1,2,3,4,5,6], 2), [2,4,6]);
+console.log(divisibleBy([1,2,3,4,5,6], 3), [3,6]);
+console.log(divisibleBy([0,1,2,3,4,5,6], 4), [0,4]);
+console.log(divisibleBy([0], 4), [0]);
+console.log(divisibleBy([1,3,5], 2), []);
+
+
+/*
+https://www.codewars.com/kata/55edaba99da3a9c84000003b/javascript
+
+
+Complete the function which takes two arguments and returns all
+numbers which are divisible by the given divisor.
+First argument is an array of numbers and the second is the divisor.
+
+Example
+divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+*/
