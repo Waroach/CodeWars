f11a2739d59308f12e3f255bf3de58d3c6c49300
waroach <brent.rankin@gmail.com>
2021-08-31 10:51:26 -0700

    Completed 8kyu Divisble-by-X

--- Javascript/8kyu/Find-numbers-which-are-divisible-by-given-number.js
+++ Javascript/8kyu/Find-numbers-which-are-divisible-by-given-number.js
@@ -1,5 +1,11 @@
 function divisibleBy(numbers, divisor){
-
+    let nums = []
+    for(const num of numbers){
+        if (num % divisor == 0){
+            nums.push(num)
+        }
+    }
+    return nums
 }
 
 console.log(divisibleBy([1,2,3,4,5,6], 2), [2,4,6]);
