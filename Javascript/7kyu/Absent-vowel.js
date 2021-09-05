47dded27df81571c3c4b37ada5bd3cc22c34d7db
waroach <brent.rankin@gmail.com>
2021-09-05 15:57:55 -0600

    Completed 7kyu Absent-Vowel

--- Javascript/7kyu/Absent-vowel.js
+++ Javascript/7kyu/Absent-vowel.js
@@ -1,5 +1,9 @@
 function absentVowel(x){
-
+    if(!x.includes("a")){return 0}
+    if(!x.includes("e")){return 1}
+    if(!x.includes("i")){return 2}
+    if(!x.includes("o")){return 3}
+    if(!x.includes("u")){return 4}
 }
 
 console.log(absentVowel("John Doe hs seven red pples under his bsket"), 0);
