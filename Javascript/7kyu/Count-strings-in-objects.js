4a8f25edb52498565f75356bdb7ea0f672d74e5c
waroach <brent.rankin@gmail.com>
2021-09-17 15:39:25 +0200

    Started 7kyu Count-Strings-on-Objects

---
+++ Javascript/7kyu/Count-strings-in-objects.js
@@ -0,0 +1,28 @@
+function strCount(obj){
+    // Your code here
+}
+
+console.log(strCount({
+    first: "1",
+    second: "2",
+    third: false,
+    fourth: ["anytime",2,3,4],
+    fifth:  null
+    }), 3)
+
+/*
+https://www.codewars.com/kata/565b3542af398bfb50000003/train/javascript
+
+
+Create a function strCount (takes an object as argument) that will count all string values inside an object.
+
+For example:
+strCount({
+    first: "1",
+    second: "2",
+    third: false,
+    fourth: ["anytime",2,3,4],
+    fifth:  null
+    })
+  //returns 3
+*/
