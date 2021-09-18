02a06762bac2c2076fe720669f7c49bddff94ec0
waroach <brent.rankin@gmail.com>
2021-09-18 10:39:57 +0200

    Started 7kyu Reverser

---
+++ Javascript/7kyu/Reverser.js
@@ -0,0 +1,24 @@
+function reverse(n){
+    // your code here
+}
+
+
+console.log(reverse(1234), 4321);
+console.log(reverse(10987), 78901);
+console.log(reverse(1020), 201);
+
+/*
+https://www.codewars.com/kata/58069e4cf3c13ef3a6000168/train/javascript
+
+
+Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+
+// Please do not use
+const forbidden = "
+    '
+    `
+    string
+    fixed
+    precision
+    .keys
+*/
