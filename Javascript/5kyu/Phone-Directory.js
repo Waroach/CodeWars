09b47479523f56b1422646ac36041e76fe4f5848
waroach <brent.rankin@gmail.com>
2021-10-03 15:27:13 +0200

    Need to redo this one

--- Javascript/5kyu/Phone-Directory.js
+++ Javascript/5kyu/Phone-Directory.js
@@ -12,26 +12,31 @@ const dr = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kus
 // DO NOT TOUCH ABOVE
 
 // (<.+>)|(\d+\-\d+\-\d+\-\d+)
-
+// Need to redo this
 function phone(str, num) {
     let arr = str.split`\n`.map(v=>{
         let a = []
         let regexNumber = /\d+\-\d+\-\d+\-\d+/
         let regexName = /<.+>/
+
         v=v.replace(regexNumber,x=>{
         a.push(x)
         return ''
         })
+        
         v=v.replace(regexName,x=>{
         a.push(x.slice(1,-1))
         return ''
         })
+
         v.replace(/.+/g,x=>{
         a.push(x.replace(/[^0-9a-zA-Z\.\-\s_]/g,'').replace(/\s+|_/g,' ').trim())
         return ''
         })
+
         return a
     })
+
     let check = arr.filter(v=>v[0]===num)
     if (check.length===0) return `Error => Not found: ${num}`
     if (check.length>1) return `Error => Too many people: ${num}`
