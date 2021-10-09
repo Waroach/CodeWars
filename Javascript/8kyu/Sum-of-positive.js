458a2b2160ff666c62c715da653e1b6a112f9250
waroach <brent.rankin@gmail.com>
2021-10-09 11:21:50 -0600

    added tests 8kyu sum-of-positive

--- Javascript/8kyu/Sum-of-positive.js
+++ Javascript/8kyu/Sum-of-positive.js
@@ -2,6 +2,12 @@ function positiveSum(arr) {
   
 }
 
+console.log(positiveSum([1,2,3,4,5]),15);
+console.log(positiveSum([1,-2,3,4,5]),13);
+console.log(positiveSum([]),0);
+console.log(positiveSum([-1,-2,-3,-4,-5]),0);
+console.log(positiveSum([-1,2,3,4,-5]),9);
+
 /*
 https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
 
