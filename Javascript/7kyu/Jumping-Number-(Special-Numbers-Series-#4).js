e45b4fd8cb588b3e6b2f899dff6eea8b6660217c
waroach <brent.rankin@gmail.com>
2021-10-22 13:20:39 -0600

    Added Test Cases 7kyu Jumping-Number-SNS#4

--- Javascript/7kyu/Jumping-Number-(Special-Numbers-Series-#4).js
+++ Javascript/7kyu/Jumping-Number-(Special-Numbers-Series-#4).js
@@ -12,6 +12,18 @@ function balancedNum(n){
     return 'Not Balanced'
 }
 
+console.log(jumpingNumber(1), "Jumping!!");
+console.log(jumpingNumber(7), "Jumping!!");
+console.log(jumpingNumber(9), "Jumping!!");
+console.log(jumpingNumber(23), "Jumping!!");
+console.log(jumpingNumber(32), "Jumping!!");
+console.log(jumpingNumber(79), "Not!!");
+console.log(jumpingNumber(98), "Jumping!!");
+console.log(jumpingNumber(8987), "Jumping!!");
+console.log(jumpingNumber(4343456), "Jumping!!");
+console.log(jumpingNumber(98789876), "Jumping!!");
+
+
 /*
 https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed/train/javascript
 
