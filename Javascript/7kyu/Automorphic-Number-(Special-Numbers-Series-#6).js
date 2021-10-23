7192612736dbd54d0ce654f84ad79830a4432b71
waroach <brent.rankin@gmail.com>
2021-10-23 09:37:40 -0600

    Added Test Cases 7kyu Automorphic-Number-SNS#6

--- Javascript/7kyu/Automorphic-Number-(Special-Numbers-Series-#6).js
+++ Javascript/7kyu/Automorphic-Number-(Special-Numbers-Series-#6).js
@@ -2,6 +2,17 @@ function automorphic(n){
     //your code here
 }
 
+console.log(automorphic(1),"Automorphic");
+console.log(automorphic(3),"Not!!");
+console.log(automorphic(6),"Automorphic");
+console.log(automorphic(9),"Not!!");
+console.log(automorphic(25),"Automorphic");
+console.log(automorphic(53),"Not!!");
+console.log(automorphic(76),"Automorphic");
+console.log(automorphic(95),"Not!!");
+console.log(automorphic(625),"Automorphic");
+console.log(automorphic(225),"Not!!");
+
 
 /*
 https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
