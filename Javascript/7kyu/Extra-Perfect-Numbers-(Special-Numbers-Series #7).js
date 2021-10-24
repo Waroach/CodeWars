c088a6d21ae2beb02c7707aa28106eabf9c1863c
waroach <brent.rankin@gmail.com>
2021-10-24 08:52:10 -0600

    Added Test Cases Extra-Perfect-Numbers-SNS#7

--- Javascript/7kyu/Extra-Perfect-Numbers-(Special-Numbers-Series #7).js
+++ Javascript/7kyu/Extra-Perfect-Numbers-(Special-Numbers-Series #7).js
@@ -3,6 +3,13 @@ function extraPerfect(n){
 }
 
 
+console.log(extraPerfect(3), [1,3]);
+console.log(extraPerfect(5), [1,3,5]);
+console.log(extraPerfect(7), [1,3,5,7]);
+console.log(extraPerfect(28), [1,3,5,7,9,11,13,15,17,19,21,23,25,27]);
+console.log(extraPerfect(39), [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]);
+
+
 /*
 https://www.codewars.com/kata/5a662a02e626c54e87000123/train/javascript
 
