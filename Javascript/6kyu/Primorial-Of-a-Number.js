324e0272b48894dc82e947b181b872268594c420
waroach <brent.rankin@gmail.com>
2021-10-25 14:57:32 -0600

    Completed 6kyu Primorial-Of-a-Number

--- Javascript/6kyu/Primorial-Of-a-Number.js
+++ Javascript/6kyu/Primorial-Of-a-Number.js
@@ -1,6 +1,18 @@
 function numPrimorial(n){
-    //your code here
-}
+    let arr = [];
+    for (let i=1;;i++){
+      if (n===arr.length){break}
+      if (isPrime(i)) arr.push(i)
+    }
+    return arr.reduce((a,b)=>a*b,1)
+  }
+  function isPrime(n) {
+    let rt = Math.sqrt(n);
+    for(let i = 2; i <= rt; i++) {
+      if(n % i === 0) return false; 
+    }
+    return n !== 1;
+  }
 
 
 console.log(numPrimorial(3),30);
