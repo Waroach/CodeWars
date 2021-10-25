8c00a338dab5a9b1ee770e195d0f69159bf85ef1
waroach <brent.rankin@gmail.com>
2021-10-25 08:09:43 -0600

    Added Case Notes 6kyu Sum-of-Two-Integers

--- Javascript/6kyu/Sum-of-Two-Integers.js
+++ Javascript/6kyu/Sum-of-Two-Integers.js
@@ -4,6 +4,22 @@ function add(x, y)
 }
 
 
+// Test Positive
+Test.assertEquals(add(1, 2), 3)
+Test.assertEquals(add(5,19), 24)
+Test.assertEquals(add(23,17), 40)
+
+// Test Negative
+Test.assertEquals(add(-14,-16), -30);
+Test.assertEquals(add(-50,-176), -226);
+Test.assertEquals(add(-10,-29), -39);
+
+// Test Mix
+Test.assertEquals(add(-13,13), 0);
+Test.assertEquals(add(-27,18), -9);
+Test.assertEquals(add(-90,30), -60);
+
+
 /*
 https://www.codewars.com/kata/5a9c35e9ba1bb5c54a0001ac/train/javascript
 
