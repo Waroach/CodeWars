59d0dae2c3b3e2f7d905ba66cadce1b387679d1f
waroach <brent.rankin@gmail.com>
2021-10-25 08:23:34 -0600

    Created One Line 6kyu Sum-of-Two-Integers

--- Javascript/6kyu/Sum-of-Two-Integers.js
+++ Javascript/6kyu/Sum-of-Two-Integers.js
@@ -1,6 +1,10 @@
-function add(x, y){
-    return x + y
-}
+const add = (x, y) => x + y
+
+
+                            // First Try
+// function add(x, y){
+//     return x + y
+// }
 
 
 // Test Positive
