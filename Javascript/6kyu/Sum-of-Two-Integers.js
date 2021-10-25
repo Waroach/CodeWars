1b7cc319b3aaf779039194f4b56fb1f56b68d4d9
waroach <brent.rankin@gmail.com>
2021-10-25 08:10:20 -0600

    Added Test Cases 6kyu Sum-of-Two-Integers

--- Javascript/6kyu/Sum-of-Two-Integers.js
+++ Javascript/6kyu/Sum-of-Two-Integers.js
@@ -5,19 +5,19 @@ function add(x, y)
 
 
 // Test Positive
-Test.assertEquals(add(1, 2), 3)
-Test.assertEquals(add(5,19), 24)
-Test.assertEquals(add(23,17), 40)
+console.log(add(1, 2), 3)
+console.log(add(5,19), 24)
+console.log(add(23,17), 40)
 
 // Test Negative
-Test.assertEquals(add(-14,-16), -30);
-Test.assertEquals(add(-50,-176), -226);
-Test.assertEquals(add(-10,-29), -39);
+console.log(add(-14,-16), -30);
+console.log(add(-50,-176), -226);
+console.log(add(-10,-29), -39);
 
 // Test Mix
-Test.assertEquals(add(-13,13), 0);
-Test.assertEquals(add(-27,18), -9);
-Test.assertEquals(add(-90,30), -60);
+console.log(add(-13,13), 0);
+console.log(add(-27,18), -9);
+console.log(add(-90,30), -60);
 
 
 /*
