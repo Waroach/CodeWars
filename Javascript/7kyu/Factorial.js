a6ca483cdfb0986d598efe12025b0a3adee44cc3
waroach <brent.rankin@gmail.com>
2021-10-26 10:06:32 -0600

    Added One Liner Cases 7kyu Factorial

--- Javascript/7kyu/Factorial.js
+++ Javascript/7kyu/Factorial.js
@@ -1,7 +1,11 @@
-function factorial(n){
-    if(n===0) return 1
-    return n * factorial(n-1)
-}
+const factorial = n => n==0 ? 1 : n * factorial(n-1)
+
+
+                            // First try with recursion
+// function factorial(n){
+//     if(n===0) return 1
+//     return n * factorial(n-1)
+// }
 
 
 console.log(factorial(0), 1);
