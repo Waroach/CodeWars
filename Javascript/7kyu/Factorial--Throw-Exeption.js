0f385d925ebf162fbcbfddfa67606d6b817cb9e9
waroach <brent.rankin@gmail.com>
2021-10-26 10:22:08 -0600

    Completed 7kyu Factorial-Throw-Exeption

--- Javascript/7kyu/Factorial--Throw-Exeption.js
+++ Javascript/7kyu/Factorial--Throw-Exeption.js
@@ -1,6 +1,6 @@
-function factorial(n)
-{
-  // Calculate the factorial here
+function factorial(n){
+  if(n<0 || n>12) throw new RangeError("My Math sucks stay between 0 and 12.")
+  return n === 0 ? 1 : n * factorial(n-1)
 }
 
 
