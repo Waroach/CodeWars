fb262750966d46ab7c14de73cb5a39bc2308fbbf
waroach <brent.rankin@gmail.com>
2021-10-26 09:33:57 -0600

    Added Test Cases 7kyu Factorial

--- Javascript/7kyu/Factorial.js
+++ Javascript/7kyu/Factorial.js
@@ -2,6 +2,14 @@ function factorial(n){
     //your code here
 }
 
+
+console.log(factorial(0), 1);
+console.log(factorial(1), 1);
+console.log(factorial(4), 24);
+console.log(factorial(7), 5040);
+console.log(factorial(17), 355687428096000);
+
+
 /*
 https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
 
