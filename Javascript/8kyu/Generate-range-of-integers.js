4aa1676d0b4dcdc3c6ae7c4f4c9c40350745414e
waroach <brent.rankin@gmail.com>
2021-10-30 09:46:09 -0600

    Added Test Cases 8kyu Generate-range-of-integers

--- Javascript/8kyu/Generate-range-of-integers.js
+++ Javascript/8kyu/Generate-range-of-integers.js
@@ -3,6 +3,10 @@ function generateRange(min, max, step){
 }
 
 
+console.log(generateRange(2, 10, 2), [2,4,6,8,10])
+console.log(generateRange(1, 10, 3), [1,4,7,10])
+
+
 
 /*
 https://www.codewars.com/kata/55eca815d0d20962e1000106/train/javascript
