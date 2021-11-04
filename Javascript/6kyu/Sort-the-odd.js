3b785b603ac363f367529d3dfe630f507023cb07
waroach <brent.rankin@gmail.com>
2021-11-04 07:29:19 -0600

    Redid 6kyu Sort-The-Odds

--- Javascript/6kyu/Sort-the-odd.js
+++ Javascript/6kyu/Sort-the-odd.js
@@ -1,7 +1,14 @@
-function sortArray(arr) {
-    const oddNums = arr.filter((x) => x % 2).sort((a,b)=>a-b)
-    return arr.map(n => n % 2 ? oddNums.shift() : n)
+function sortArray(array) {
+    const odd = array.filter(x => x%2).sort((a,b)=>a-b)
+    return array.map(nums=> nums%2 ? console.log( odd.shift(), nums) : nums)
 }
+
+
+                            // First try
+// function sortArray(arr) {
+//     const oddNums = arr.filter((x) => x % 2).sort((a,b)=>a-b)
+//     return arr.map(n => n % 2 ? oddNums.shift() : n)
+// }
 
 console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
 console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
