183de7adc027cd5ddf2e2bc022caa13fb47c0996
waroach <brent.rankin@gmail.com>
2021-11-05 09:28:58 -0600

    Completed 8kyu Find-the-Remainder

--- Javascript/8kyu/Find-the-Remainder.js
+++ Javascript/8kyu/Find-the-Remainder.js
@@ -1,6 +1,9 @@
 function remainder(a, b){
-    // Divide the larger argument by the smaller argument and return the remainder
-    
+    if(a > b){
+        return (a % b)
+    }else{
+        return (b % a)
+    }
 }
 
 
