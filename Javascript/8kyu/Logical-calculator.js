e916c954d4c2c2eca00e7f73fa6e300cf6410b7d
waroach <brent.rankin@gmail.com>
2021-11-06 17:35:25 -0600

    Completed 8kyu Logical-Calculator

--- Javascript/8kyu/Logical-calculator.js
+++ Javascript/8kyu/Logical-calculator.js
@@ -1,6 +1,10 @@
-function logicalCalc(array, op){
-    //your code here
+const operations = {
+    AND: (a, b) => a && b,
+    OR: (a, b) => a || b,
+    XOR: (a, b) => a !== b,
 }
+
+const logicalCalc = (array, op) => array.reduce(operations[op])
 
 
 
