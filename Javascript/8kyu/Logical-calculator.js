861e2e67ddc4547717b0e9e8eba856b558a82279
waroach <brent.rankin@gmail.com>
2021-11-06 17:18:10 -0600

    Added Test Cases 8kyu Logical-Calculator

--- Javascript/8kyu/Logical-calculator.js
+++ Javascript/8kyu/Logical-calculator.js
@@ -3,6 +3,31 @@ function logicalCalc(array, op){
 }
 
 
+
+console.log(logicalCalc([true, true, true, false], "AND"), false);
+console.log(logicalCalc([true, true, true, false], "OR"), true);
+console.log(logicalCalc([true, true, true, false], "XOR"), true);
+console.log(logicalCalc([true, true, false, false], "AND"), false);
+console.log(logicalCalc([true, true, false, false], "OR"), true);
+console.log(logicalCalc([true, true, false, false], "XOR"), false);
+console.log(logicalCalc([true, false, false, false], "AND"), false);
+console.log(logicalCalc([true, false, false, false], "OR"), true);
+console.log(logicalCalc([true, false, false, false], "XOR"), true);
+console.log(logicalCalc([true, true], "AND"), true);
+console.log(logicalCalc([true, true], "OR"), true);
+console.log(logicalCalc([true, true], "XOR"), false);
+console.log(logicalCalc([false, false], "AND"), false);
+console.log(logicalCalc([false, false], "OR"), false);
+console.log(logicalCalc([false, false], "XOR"), false);
+console.log(logicalCalc([false], "AND"), false);
+console.log(logicalCalc([false], "OR"), false);
+console.log(logicalCalc([false], "XOR"), false);
+console.log(logicalCalc([true], "AND"), true);
+console.log(logicalCalc([true], "OR"), true);
+console.log(logicalCalc([true], "XOR"), true);
+
+
+
 /*
 https://www.codewars.com/kata/57096af70dad013aa200007b/train/javascript
 
