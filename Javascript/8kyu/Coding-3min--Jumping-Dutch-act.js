78eefb9d641266b616eecc292d36016db086ee38
waroach <brent.rankin@gmail.com>
2021-11-08 08:47:43 -0700

    Added Test Cases 8kyu Coding-3min-Jumping-Dutch-In

--- Javascript/8kyu/Coding-3min--Jumping-Dutch-act.js
+++ Javascript/8kyu/Coding-3min--Jumping-Dutch-act.js
@@ -3,6 +3,13 @@ function sc(floor){
 }
 
 
+console.log(sc(2), "Aa~ Pa! Aa!", "good luck!");  
+console.log(sc(6), "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!", "good luck!"); 
+console.log(sc(7), "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!", "good luck!"); 
+console.log(sc(10), "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!", "good luck!"); 
+console.log(sc(1), "", "good luck!"); 
+console.log(sc(-1), "", "good luck!"); 
+
 
 /*
 https://www.codewars.com/kata/570bcd9715944a2c8e000009/train/javascript
