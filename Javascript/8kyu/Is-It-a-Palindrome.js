2b68d3e19fd246e6c4294df6efc57461597cb8a7
waroach <brent.rankin@gmail.com>
2021-11-09 07:50:29 -0700

    Added Test Cases 8 kyu is-it-a-palindrome

--- Javascript/8kyu/Is-It-a-Palindrome.js
+++ Javascript/8kyu/Is-It-a-Palindrome.js
@@ -2,6 +2,19 @@ function isPalindrome(x) {
     // your code here
 }
 
+
+
+it("Testing for 'a'", () => assert.strictEqual(isPalindrome("a"), true));
+it("Testing for 'aba'", () => assert.strictEqual(isPalindrome("aba"), true));
+it("Testing for 'Abba'", () => assert.strictEqual(isPalindrome("Abba"), true));
+it("Testing for 'hello'", () => assert.strictEqual(isPalindrome("hello"), false));
+it("Testing for 'Bob'", () => assert.strictEqual(isPalindrome("Bob"), true));
+it("Testing for 'Madam'", () => assert.strictEqual(isPalindrome("Madam"), true));
+it("Testing for 'AbBa'", () => assert.strictEqual(isPalindrome("AbBa"), true));
+it("Testing for ''", () => assert.strictEqual(isPalindrome(""), true));
+
+
+
 /*
 https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
 
