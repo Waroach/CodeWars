3ba719613028363732664dda9894270cb914385c
waroach <brent.rankin@gmail.com>
2021-11-09 07:49:58 -0700

    Started 8 kyu is-it-a-palindrome

---
+++ Javascript/8kyu/Is-It-a-Palindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(x) {
+    // your code here
+}
+
+/*
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+
+
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+*/
