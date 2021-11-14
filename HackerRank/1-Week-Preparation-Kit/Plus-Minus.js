dcae149789bf6e5041a6bb8e9704dfcbe07ff412
waroach <brent.rankin@gmail.com>
2021-11-14 09:21:57 -0700

    worked on hackerRank-OneWeekDayOne-PlusMinus

--- HackerRank/1-Week-Preparation-Kit/Plus-Minus.js
+++ HackerRank/1-Week-Preparation-Kit/Plus-Minus.js
@@ -15,7 +15,7 @@ function plusMinus(arr) {
     return result
 }
 
-// this comment will give a push
+
 
 /*
 https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
