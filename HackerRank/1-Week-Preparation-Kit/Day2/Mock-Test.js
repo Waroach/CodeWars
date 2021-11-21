e1b261a10f0a66809a5c2624b1b09e07c5baea12
waroach <brent.rankin@gmail.com>
2021-11-21 10:49:02 -0700

    Worked on Mock-Test Day2-1WPK

--- HackerRank/1-Week-Preparation-Kit/Day2/Mock-Test.js
+++ HackerRank/1-Week-Preparation-Kit/Day2/Mock-Test.js
@@ -1,12 +1,79 @@
-function flippingMatrix(matrix) {
+                            // I need to check this.
 
-    
+
+function flippingMatrix(matrix) {
+    let total = 0
+    let temptotal = 0
+    // Normal Check
+    tempMatrix = matrix
+    maxTotal()
+    // Flip first row
+    tempMatrix[0] = tempMatrix[0].reverse()
+    maxTotal()
+    // Flip Second Row
+    tempMatrix[1] = tempMatrix[1].reverse()
+    maxTotal()
+    // Reset
+    tempMatrix = matrix
+
+    // flip third column
+    tempMatrix[0][2] = tempMatrix[3][2]
+    tempMatrix[1][2] = tempMatrix[2][2]
+    //Flip first row
+    tempMatrix[0] = tempMatrix[0].reverse()
+    maxTotal()
+    // Reset
+    tempMatrix[0] = tempMatrix[0].reverse()
+    // Flip Second Row
+    tempMatrix[1] = tempMatrix[1].reverse()
+    maxTotal()
+    // Flip First and Second Row
+    tempMatrix[0] = tempMatrix[0].reverse()
+    maxTotal()
+    // RESET
+    tempMatrix = matrix
+
+    // flip fourth column
+    tempMatrix[0][3] = tempMatrix[3][3]
+    tempMatrix[1][3] = tempMatrix[2][3]
+    //Flip first row
+    tempMatrix[0] = tempMatrix[0].reverse()
+    maxTotal()
+    // Reset
+    tempMatrix[0] = tempMatrix[0].reverse()
+    // Flip Second Row
+    tempMatrix[1] = tempMatrix[1].reverse()
+    maxTotal()
+    // Flip First and Second Row
+    tempMatrix[0] = tempMatrix[0].reverse()
+    maxTotal()
+    // RESET
+    tempMatrix = matrix
+
+    // flip third and fourth
+    // flip third column
+    tempMatrix[0][2] = tempMatrix[3][2]
+    tempMatrix[1][2] = tempMatrix[2][2]
+    // Flip Fourth column
+    tempMatrix[0][3] = tempMatrix[3][3]
+    tempMatrix[1][3] = tempMatrix[2][3]
+    tempMatrix[0] = tempMatrix[0].reverse()
+    tempMatrix[1] = tempMatrix[1].reverse()
+    maxTotal()
+
+
+    function maxTotal(){
+        if(total < (tempMatrix[0][0] + tempMatrix[0][1] + tempMatrix[1][0] + tempMatrix[1][1])){
+            total = tempMatrix[0][0] + tempMatrix[0][1] + tempMatrix[1][0] + tempMatrix[1][1]
+        }
+    }
 }
 
 flippingMatrix( [[112, 42, 83, 119],[56, 125, 56, 49],[15, 78, 101, 43],[62, 98, 114, 108]] )
 
 
 /*
+https://www.hackerrank.com/interview/preparation-kits/one-week-preparation-kit/one-week-day-two/challenges
 
 
 We Start Out with the follwing 2n x 2n matrix
