fb9c42c1a32796b51c7ff66dd205063b576c3a3e
waroach <brent.rankin@gmail.com>
2021-11-22 11:05:43 -0700

    Started 7kyu Grains

---
+++ Javascript/7kyu/Grains.js
@@ -0,0 +1,16 @@
+function square(number){
+    //your code here
+}
+
+
+/*
+https://www.codewars.com/kata/55f7eb009e6614447b000099/train/javascript
+
+
+Grains
+Write a program that calculates the number of grains of wheat on a specific
+square of chessboard given that the number on each square is double the previous one.
+There are 64 squares on a chessboard.
+#Example: square(1) = 1 square(2) = 2 square(3) = 4 square(4) = 8 etc...
+Write a program that shows how many grains were on each square.
+*/
