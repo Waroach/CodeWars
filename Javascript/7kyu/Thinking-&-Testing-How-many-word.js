f8cc2f7390e02dbdcae3a6f52388f683afc12223
waroach <brent.rankin@gmail.com>
2021-11-26 00:05:35 -0700

    Started 7kyu Thinking-and-Testing-How-Many-Word

---
+++ Javascript/7kyu/Thinking-&-Testing-How-many-word.js
@@ -0,0 +1,15 @@
+function testit(s){
+    return ?
+}
+
+
+
+/*
+https://www.codewars.com/kata/56eff1e64794404a720002d2/train/javascript
+
+
+No Story
+No Description
+Only by Thinking and Testing
+Look at result of testcase, guess the code!
+*/
