125727d9a3e9b194f692ea5f3beea7bc6acf694d
waroach <brent.rankin@gmail.com>
2021-11-29 18:12:27 -0700

    Started 7kyu Exclamation-Marks-Series-8

---
+++ Javascript/6kyu/Exclamation-marks-series-#8-Move-all-exclamation-marks-to-the-end-of-the-sentence.js
@@ -0,0 +1,21 @@
+function remove(s){
+    //coding and coding....
+    
+    
+}
+
+
+
+/*
+https://www.codewars.com/kata/57fafd0ed80daac48800019f/train/javascript
+
+
+Description:
+Move all exclamation marks to the end of the sentence
+Examples
+remove("Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!!"
+remove("Hi! Hi! Hi!") === "Hi Hi Hi!!!"
+remove("Hi! !Hi Hi!") === "Hi Hi Hi!!!"
+remove("Hi! Hi!! Hi!") === "Hi Hi Hi!!!!"
+*/
