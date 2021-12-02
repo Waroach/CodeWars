46be377b04d13edef7cd22a3904a2acd5f16e63b
waroach <brent.rankin@gmail.com>
2021-12-01 18:08:02 -0700

    Completed 7kyu Hello-Wold-Without-Strings

--- Javascript/6kyu/Hello-World--Without-Strings.js
+++ Javascript/6kyu/Hello-World--Without-Strings.js
@@ -1,6 +1,27 @@
 const helloWorld = () => {
-    return new String('Hello, World!')
+    let hello = {
+        '0': 'H',
+        '1': 'e',
+        '2': 'l',
+        '3': 'l',
+        '4': 'o',
+        '5': ',',
+        '6': ' ',
+        '7': 'W',
+        '8': 'o',
+        '9': 'r',
+        '10': 'l',
+        '11': 'd',
+        '12': '!',
+    }
+    return Object.values(hello).join('')
 }
+
+
+                            // FIRST TRY || NOT WORKING
+// const helloWorld = () => {
+//     return new String('Hello, World!')
+// }
 
 
 console.log(helloWorld(), "Hello, World!")
