821aee0593b43e562953f107010379e16ed0b6f2
waroach <brent.rankin@gmail.com>
2021-12-02 17:40:23 -0700

    Added Test Cases 7kyu Longest-vowel-chain

--- Javascript/7kyu/Longest-vowel-chain.js
+++ Javascript/7kyu/Longest-vowel-chain.js
@@ -4,6 +4,14 @@ function solve(s){
 
 
 
+console.log(solve("codewarriors"),2);
+console.log(solve("suoidea"),3);
+console.log(solve("ultrarevolutionariees"),3);
+console.log(solve("strengthlessnesses"),1);
+console.log(solve("cuboideonavicuare"),2);
+console.log(solve("chrononhotonthuooaos"),5);
+console.log(solve("iiihoovaeaaaoougjyaw"),8);
+
 
 
 /*
