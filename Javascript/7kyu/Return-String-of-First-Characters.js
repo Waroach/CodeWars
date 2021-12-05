e703d8c7bf4110f59252e98ae06711c5788810f5
waroach <brent.rankin@gmail.com>
2021-12-05 15:31:57 -0700

    Completed 7kyu Return-String-of-First-Characters

--- Javascript/7kyu/Return-String-of-First-Characters.js
+++ Javascript/7kyu/Return-String-of-First-Characters.js
@@ -1,20 +1,35 @@
-function makeString(s){
-    return s.split(' ').map(e=> e[0] ).join('')
+function makeString(s) {
+  return s
+    .split(" ")
+    .map((e) => e[0])
+    .join("");
 }
 
+// PSEUDOCODE
+// Split the string into single words.
+//Take the first element out of each string from the above array.
+// return the results
 
-                            // PSEUDOCODE
-    // Split the string into single words.
-    //Take the first element out of each string from the above array.
-    // return the results
-
-
-console.log(makeString("sees eyes xray yoat"), "sexy", "Wrong result for 'sees eyes xray yoat'")
-console.log(makeString("brown eyes are nice"), "bean", "Wrong result for 'brown eyes are nice'")
-console.log(makeString("cars are very nice"), "cavn", "Wrong result for 'cars are very nice'")
-console.log(makeString("kaks de gan has a big head"), "kdghabh", "Wrong result for 'kaks de gan has a big head'")
-
-
+console.log(
+  makeString("sees eyes xray yoat"),
+  "sexy",
+  "Wrong result for 'sees eyes xray yoat'"
+);
+console.log(
+  makeString("brown eyes are nice"),
+  "bean",
+  "Wrong result for 'brown eyes are nice'"
+);
+console.log(
+  makeString("cars are very nice"),
+  "cavn",
+  "Wrong result for 'cars are very nice'"
+);
+console.log(
+  makeString("kaks de gan has a big head"),
+  "kdghabh",
+  "Wrong result for 'kaks de gan has a big head'"
+);
 
 /*
 https://www.codewars.com/kata/5639bdcef2f9b06ce800005b/train/javascript
