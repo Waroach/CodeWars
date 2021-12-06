af50f5be5c5fa11bb7ed1d30e1d2c0c4a6dec9f7
waroach <brent.rankin@gmail.com>
2021-12-06 07:30:46 -0700

    Added Test Cases 7kyu Simple-Fun-69-Are-Equally-Strong

--- Javascript/7kyu/Simple-Fun-#69-Are-Equally-Strong.js
+++ Javascript/7kyu/Simple-Fun-#69-Are-Equally-Strong.js
@@ -5,6 +5,17 @@ function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
 }
 
 
+
+console.log(areEquallyStrong(10,15,15,10),true)
+console.log(areEquallyStrong(15,10,15,10),true)
+console.log(areEquallyStrong(10,10,10,10),true)
+console.log(areEquallyStrong(15,10,15,9),false)
+console.log(areEquallyStrong(10,5,5,10),true)
+console.log(areEquallyStrong(1,10,10,0),false)
+console.log(areEquallyStrong(10, 5, 11, 4),false)
+
+
+
 /*
 https://www.codewars.com/kata/5894017082b9fb62c50000df/train/javascript
 
