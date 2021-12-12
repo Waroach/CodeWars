19f9f171e87845a47c1c7a396d871f20d89469d7
waroach <brent.rankin@gmail.com>
2021-12-12 08:51:36 -0700

    First Try 7kyu Elapsed-Seconds

--- Javascript/7kyu/Elapsed-Seconds.js
+++ Javascript/7kyu/Elapsed-Seconds.js
@@ -1,6 +1,10 @@
+
+
+                            // This is giving NAN
 function elapsedSeconds(startDate, endDate){
-  
+    return (Date(startDate) - Date(endDate))
 }
+
 
 
                             // PSEUDOCODE
