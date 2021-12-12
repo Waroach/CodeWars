389df29cea022a946a52f0abe7497a9c900171f6
waroach <brent.rankin@gmail.com>
2021-12-12 09:20:57 -0700

    Added Test Cases 7kyu Simple-Fun-74-Growing-Plant

--- Javascript/7kyu/Simple-Fun-#74-Growing-Plant.js
+++ Javascript/7kyu/Simple-Fun-#74-Growing-Plant.js
@@ -6,6 +6,10 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
 
 
 
+console.log(growingPlant(100,10,910),10)
+console.log(growingPlant(10,9,4),1)
+
+
 
 /*
 https://www.codewars.com/kata/58941fec8afa3618c9000184/train/javascript
