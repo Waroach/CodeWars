0ebe44dbdc7fffe3e2c105c6725b751027b96d01
waroach <brent.rankin@gmail.com>
2021-12-16 11:52:03 -0700

    Added Test Cases 7kyu Unlucky-Days

--- Javascript/7kyu/Unlucky-Days.js
+++ Javascript/7kyu/Unlucky-Days.js
@@ -3,6 +3,20 @@ function unluckyDays(year){
 }
 
 
+
+console.log(unluckyDays(1586), 1, "should be: 1");
+console.log(unluckyDays(1001), 3, "should be: 3");
+console.log(unluckyDays(2819), 2, "should be: 2");
+console.log(unluckyDays(2792), 2, "should be: 2");
+console.log(unluckyDays(2723), 2, "should be: 2");
+console.log(unluckyDays(1909), 1, "should be: 1");
+console.log(unluckyDays(1812), 2, "should be: 2");
+console.log(unluckyDays(1618), 2, "should be: 2");
+console.log(unluckyDays(2132), 1, "should be: 1");
+console.log(unluckyDays(2065), 3, "should be: 3");
+
+
+
 /*
 https://www.codewars.com/kata/56eb0be52caf798c630013c0/train/javascript
 
