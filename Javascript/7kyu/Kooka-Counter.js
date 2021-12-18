a33e415ceb3ea5ff621c6ac671237aedad73ad42
waroach <brent.rankin@gmail.com>
2021-12-18 11:38:44 -0700

    Started 7kyu Kooka-counter

---
+++ Javascript/7kyu/Kooka-Counter.js
@@ -0,0 +1,21 @@
+var kookaCounter = function(laughing) {
+    // Your code here!
+    return 0
+}
+
+
+
+/*
+https://www.codewars.com/kata/58e8cad9fd89ea0c6c000258/train/javascript
+
+
+A family of kookaburras are in my backyard.
+I can't see them all, but I can hear them!
+How many kookaburras are there?
+Hint
+The trick to counting kookaburras is to listen carefully
+The males go HaHaHa...
+The females go hahaha...
+And they always alternate male/female
+^ Kata Note : No validation is necessary; only valid input will be passed :-)
+*/
