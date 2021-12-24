9d886b258b0dfe3a3d78ff3762ae9c7f99bf93db
waroach <brent.rankin@gmail.com>
2021-12-24 05:31:05 -0700

    Added Test Cases 7kyu CPS#1-Fill-the-Hard-Disk-Drive

--- Javascript/7kyu/Computer-problem-series-#1-Fill-the-Hard-Disk-Drive.js
+++ Javascript/7kyu/Computer-problem-series-#1-Fill-the-Hard-Disk-Drive.js
@@ -4,6 +4,17 @@ function save(sizes, hd) {
 
 
 
+console.log(save([4, 4, 4, 3, 3], 12), 3);
+console.log(save([4, 4, 4, 3, 3], 11), 2);
+console.log(save([4, 8, 15, 16, 23, 42], 108), 6);
+console.log(save([13], 13), 1);
+console.log(save([1, 2, 3, 4], 250), 4);
+console.log(save([100], 500), 1);
+console.log(save([11, 13, 15, 17, 19], 8), 0);
+console.log(save([45], 12), 0);
+
+
+
 /*
 https://www.codewars.com/kata/5d49c93d089c6e000ff8428c/train/javascript
 
