d80054c40ae3a5fd6ad91f818157edd0d0f7211b
waroach <brent.rankin@gmail.com>
2021-12-28 04:07:11 -0700

    Started 7kyu Ones-Complement

---
+++ Javascript/7kyu/Ones'-Complement.js
@@ -0,0 +1,15 @@
+function onesComplement(n) {
+    // your code here
+};
+
+
+
+/*
+https://www.codewars.com/kata/59b11f57f322e5da45000254/train/javascript
+
+
+The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s. For example:
+onesComplement(1001) = 0110
+onesComplement(1001) = 0110
+For any given binary number,formatted as a string, return the Ones' Complement of that number.
+*/
