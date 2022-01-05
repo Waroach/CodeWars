b33b9d05ad731eaaec1054fb2bbbac288711128f
waroach <brent.rankin@gmail.com>
2022-01-05 07:14:38 -0700

    Added Test Cases 6kyu kingdoms-Ep-4-The-Dancing-Witch

--- Javascript/6kyu/Kingdoms-Ep-4-The-Dancing-Witch.js
+++ Javascript/6kyu/Kingdoms-Ep-4-The-Dancing-Witch.js
@@ -4,6 +4,24 @@ function figureOut(arr) {
 }
 
 
+// Normal
+console.log(figureOut(["a b", " ba"]), "a");
+console.log(figureOut(["ab kl", "ba kl", "a blk"]), "b");
+console.log(figureOut(["icwth", "wicth", "witch"]), "w");
+console.log(figureOut(["abcdef", "abcfde"]), "f");
+console.log(figureOut(["a  b  c", "a b  c ", " ba   c"]), "a");
+console.log(figureOut(["bc  x", " bcx ", " xbc "]), "x");
+console.log(figureOut(["abcd", "cabd"]), "c");
+console.log(figureOut(["xyz", "yxz", "zyx"]), "z");
+console.log(figureOut([" x y z ", " xy  z ", "yx   z ", " xy  z "]), "y");
+console.log(figureOut(["hop", "hpo", "pho"]), null);
+
+// New Edge
+console.log(figureOut(["abdgefc", "adbegcf", "daebgfc", "adegfcb", "aedfgbc", "adegfcb", "dagecfb", "adegfcb", "adgefbc"]), "b");
+console.log(figureOut(["dac   b ", "acd    b", "adc    b", "acd   b ", "ca d   b", "c ad  b ", "c ad  b ", " ca db  ", " ac bd  "]), "d");
+console.log(figureOut(["cb feg da", "bcf ge ad", "cbfg ea d", "cfb gae d", "fcbga e d", "fbcag  ed", "bfac ged ", "baf cgde "]), null);   
+
+
 
 /*
 https://www.codewars.com/kata/6171a85207ab6b003fadc43e/train/javascript
