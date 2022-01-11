227e39d171fc5c4a60ffc9323baa17bcd1582ec5
waroach <brent.rankin@gmail.com>
2022-01-10 19:09:19 -0700

    Added Test Cases 8kyu Retrun-Two-Highest-Values-in-List

--- Javascript/8kyu/Return-Two-Highest-Values-in-List.js
+++ Javascript/8kyu/Return-Two-Highest-Values-in-List.js
@@ -4,6 +4,12 @@ function twoHighest(arr) {
 
 
 
+console.log(twoHighest([]), [])
+console.log(twoHighest([15]), [15])
+console.log(twoHighest([15, 20, 20, 17]), [20, 17])
+
+
+
 /*
 https://www.codewars.com/kata/57ab3c09bb994429df000a4a/train/javascript
 
