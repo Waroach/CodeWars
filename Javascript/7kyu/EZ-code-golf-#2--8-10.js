5408ca6be9e93479457981491899ee580fe505ae
waroach <brent.rankin@gmail.com>
2022-01-11 18:25:15 -0700

    Started 7kyu EZ-Golf-#8-8-10

---
+++ Javascript/7kyu/EZ-code-golf-#2--8-10.js
@@ -0,0 +1,11 @@
+// your code here
+
+
+
+/*
+https://www.codewars.com/kata/6173bba47e5aa0000f86b76b/train/javascript
+
+
+You have to write a function f which takes in an octal numberin string
+format and output its decimal representation in less than 15 characters.
+*/
