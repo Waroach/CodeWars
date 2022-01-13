6f2bf061e3d0dd9f06e4ac7e9e55a1e2381229d2
waroach <brent.rankin@gmail.com>
2022-01-12 22:22:03 -0700

    Completed 7kyu Minimum-Steps--Array-Series-6

--- Javascript/7kyu/Minimum-Steps-(Array-Series-#6).js
+++ Javascript/7kyu/Minimum-Steps-(Array-Series-#6).js
@@ -1,5 +1,10 @@
 function minimumSteps(numbers, value){
-    //your code here
+    numbers = numbers.sort((a,b)=>a-b)
+    let total = 0
+    for(let i=0;i<numbers.length;i++){
+        total += numbers[i]
+        if(total >= value)return i
+    }
 }
 
 
