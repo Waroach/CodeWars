af53837239a63d0e5dc710e4b682206e4f7b046b
waroach <brent.rankin@gmail.com>
2022-01-16 10:14:01 -0700

    Added Test Cases 7kyu Collision-Detection

--- Javascript/7kyu/Collision-Detection.js
+++ Javascript/7kyu/Collision-Detection.js
@@ -4,6 +4,15 @@ function collision(x1, y1, radius1, x2, y2, radius2) {
 
 
 
+console.log(collision(1, 1, 1, 1.1, 1.1, 0.1), true, "Expected true.");
+console.log(collision(-1, 1, 10, -10.1, 1.1, 1), true, "Expected true.");
+console.log(collision(-5, 5, 5.0001, 5, -5, 5*Math.sqrt(5)), true, "Expected true.");
+console.log(collision(1, 1, 0.01, 1, 1.1, 0.01), false, "Expected false.");
+console.log(collision(-1, 1, 6, -10.1, 1.1, 1), false, "Expected false.");
+console.log(collision(-5, 5, 5.0001, 5, -5, 4*Math.sqrt(5)), false, "Expected false.");
+
+
+
 /*
 https://www.codewars.com/kata/599da159a30addffd00000af/train/javascript
 
