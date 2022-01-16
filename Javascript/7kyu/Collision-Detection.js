6b457996e08d039c5c934c8ffaa419779d4c48dd
waroach <brent.rankin@gmail.com>
2022-01-16 10:13:18 -0700

    Started 7kyu Collision-Detection

---
+++ Javascript/7kyu/Collision-Detection.js
@@ -0,0 +1,19 @@
+function collision(x1, y1, radius1, x2, y2, radius2) {
+    // collision?
+}
+
+
+
+/*
+https://www.codewars.com/kata/599da159a30addffd00000af/train/javascript
+
+
+Create a function to determine whether or not two circles are colliding.
+You will be given the position of both circles in addition to their radii:
+function collision(x1, y1, radius1, x2, y2, radius2) {
+  // collision?
+}
+If a collision is detected, return true. If not, return false.
+Here's a geometric diagram of what the circles passed in represent:
+alt text
+*/
