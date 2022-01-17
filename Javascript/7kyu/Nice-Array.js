a9c3573e1d47961eea3c7c61764522dcd0d02d97
waroach <brent.rankin@gmail.com>
2022-01-17 15:38:33 -0700

    Added Test Cases 7kyu Nice-Array

--- Javascript/7kyu/Nice-Array.js
+++ Javascript/7kyu/Nice-Array.js
@@ -4,6 +4,11 @@ function isNice(arr){
 
 
 
+console.log(isNice([2,10,9,3]),true);
+console.log(isNice([3,4,5,7]),false);
+
+
+
 /*
 https://www.codewars.com/kata/59b844528bcb7735560000a0/train/javascript
 
