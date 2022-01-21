9958737f2078a27f4af696349b035675bd86bb1a
waroach <brent.rankin@gmail.com>
2022-01-20 18:52:41 -0700

    Added Test Cases 7kyu Minimize-Sum-Of-Array--AS#1

--- Javascript/7kyu/Minimize-Sum-Of-Array--Array-Series#1.js
+++ Javascript/7kyu/Minimize-Sum-Of-Array--Array-Series#1.js
@@ -4,6 +4,12 @@ function minSum(arr) {
 
 
 
+console.log(minSum([5,4,2,3]), 22);
+console.log(minSum([12,6,10,26,3,24]), 342);
+console.log(minSum([9,2,8,7,5,4,0,6]), 74);
+
+
+
 /*
 https://www.codewars.com/kata/5a523566b3bfa84c2e00010b/train/javascript
 
