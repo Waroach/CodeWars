ad89962e5691c1f8e0c432dfe137f8412d15f366
waroach <brent.rankin@gmail.com>
2022-01-22 04:11:33 -0700

    Added Test Cases 7kyu Array-Leaders-AS#3

--- Javascript/7kyu/Array-Leaders-(Array-Series-#3).js
+++ Javascript/7kyu/Array-Leaders-(Array-Series-#3).js
@@ -4,6 +4,16 @@ var arrayLeaders = numbers => {
 
 
 
+
+console.log(arrayLeaders([1,2,3,4,0]), [4])
+console.log(arrayLeaders([16,17,4,3,5,2]), [17,5,2])
+console.log(arrayLeaders([-1,-29,-26,-2]), [-1])
+console.log(arrayLeaders([-36,-12,-27]),  [-36,-12])
+console.log(arrayLeaders([5,-2,2]), [5,2])
+console.log(arrayLeaders([0,-1,-29,3,2]),  [0,-1,3,2])
+
+
+
 /*
 https://www.codewars.com/kata/5a651865fd56cb55760000e0/train/javascript
 
