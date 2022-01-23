454aa57547266072fd76ca287480a10f6031a417
waroach <brent.rankin@gmail.com>
2022-01-23 01:17:50 -0700

    Added Test Cases 7kuyu Maximum-Gap-AS#4

--- Javascript/7kyu/Maximum-Gap-(Array-Series-#4).js
+++ Javascript/7kyu/Maximum-Gap-(Array-Series-#4).js
@@ -4,6 +4,19 @@ function maxGap (numbers){
 
 
 
+console.log(maxGap([13,10,2,9,5]),4);
+console.log(maxGap([13,3,5]),8);
+console.log(maxGap([24,299,131,14,26,25]),168);
+console.log(maxGap([-3,-27,-4,-2]),23);
+console.log(maxGap([-7,-42,-809,-14,-12]),767);
+console.log(maxGap([12,-5,-7,0,290]),278);
+console.log(maxGap([-54,37,0,64,-15,640,0]),576);
+console.log(maxGap([130,30,50]),80);
+console.log(maxGap([1,1,1]),0);
+console.log(maxGap([-1,-1,-1]),0);
+
+
+
 /*
 https://www.codewars.com/kata/5a7893ef0025e9eb50000013/train/javascript
 
