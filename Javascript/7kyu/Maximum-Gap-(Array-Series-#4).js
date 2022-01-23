d5f73d06f332e20b95b4c93f173e27c9050ad047
waroach <brent.rankin@gmail.com>
2022-01-23 01:31:48 -0700

    Completed with for loop 7kuyu Maximum-Gap-AS#4

--- Javascript/7kyu/Maximum-Gap-(Array-Series-#4).js
+++ Javascript/7kyu/Maximum-Gap-(Array-Series-#4).js
@@ -1,5 +1,10 @@
 function maxGap (numbers){
-    //your code here
+    let gap = 0
+    numbers = numbers.sort((a,b)=>a-b)
+    for(let i=1;i<=numbers.length;i++){
+        if(gap < numbers[i] - numbers[i-1]) gap = numbers[i] - numbers[i-1]
+    }
+    return gap
 }
 
 
