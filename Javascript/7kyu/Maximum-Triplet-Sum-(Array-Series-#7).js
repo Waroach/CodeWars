10f1b3833d6bfe71607b7f1d6b472f0beea3b475
waroach <brent.rankin@gmail.com>
2022-01-25 00:29:59 -0700

    PSEUDOCODE 7kyu Maximum-Triplet-Sum-AS#7

--- Javascript/7kyu/Maximum-Triplet-Sum-(Array-Series-#7).js
+++ Javascript/7kyu/Maximum-Triplet-Sum-(Array-Series-#7).js
@@ -4,6 +4,15 @@ function maxTriSum(numbers){
 
 
 
+                            // PSEUDOCODE
+// Sort the numbers largest to smallest
+// Remove duplicates
+// Splice the numbers array leaving the first three elements
+// Total the three numbers
+// return the total
+
+
+
 console.log(maxTriSum([3,2,6,8,2,3]),17);
 console.log(maxTriSum([2,9,13,10,5,2,9,5]),32);
 console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]),18);
