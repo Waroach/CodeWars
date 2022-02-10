b0f2d5196b7bdf1244332cca1ace4abb4d9ef6f9
waroach <brent.rankin@gmail.com>
2022-02-10 11:10:29 -0700

    Started 6kyu EZ-code-golf-#1

---
+++ Javascript/7kyu/EZ-code-golf-#1-∞.js
@@ -0,0 +1,10 @@
+// your code here
+
+
+
+/*
+https://www.codewars.com/kata/6162bc81ec0bee001bde10b3/train/javascript
+
+
+You have to write a function f which returns Infinity in less than 9 characters.
+*/
