17a767dad9beb55a638147b23923476053ddaa36
waroach <brent.rankin@gmail.com>
2022-02-10 11:30:13 -0700

    PSEUDOCODE 6kyu EZ-code-golf-#1

--- Javascript/7kyu/EZ-code-golf-#1-∞.js
+++ Javascript/7kyu/EZ-code-golf-#1-∞.js
@@ -2,6 +2,13 @@
 
 
 
+// PSEUDOCODE
+// it expects Infinity to be returned.
+// using less then 9 chars.
+// return ∞
+
+
+
 console.log(f(), Infinity, "Oops! Looks like you returned an incorrect result.")
 
 
@@ -10,5 +17,6 @@ console.log(f(), Infinity, "Oops! Looks like you returned an incorrect result.")
 https://www.codewars.com/kata/6162bc81ec0bee001bde10b3/train/javascript
 
 
+Expects ∞
 You have to write a function f which returns Infinity in less than 9 characters.
 */
