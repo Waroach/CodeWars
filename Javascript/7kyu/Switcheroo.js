505e2879ee94bec90037178eb6f9ac321a3b19cb
waroach <brent.rankin@gmail.com>
2022-02-17 05:57:14 -0700

    Added Test Cases 7kyu Switcheroo

--- Javascript/7kyu/Switcheroo.js
+++ Javascript/7kyu/Switcheroo.js
@@ -4,6 +4,12 @@ function switcheroo(x){
 
 
 
+console.log(switcheroo('abc'), 'bac');
+console.log(switcheroo('aaabcccbaaa'), 'bbbacccabbb'); 
+console.log(switcheroo('ccccc'), 'ccccc');   
+
+
+
 /*
 https://www.codewars.com/kata/57f759bb664021a30300007d/train/javascript
 
