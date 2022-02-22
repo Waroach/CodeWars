ece03217e53ff8b452bb79c35c2fba02b06ecca6
waroach <brent.rankin@gmail.com>
2022-02-22 07:30:42 -0700

    Added Test Cases 7kyu Nth-Smallest-Element-AS#4

--- Javascript/7kyu/Nth-Smallest-Element-(Array-Series-#4).js
+++ Javascript/7kyu/Nth-Smallest-Element-(Array-Series-#4).js
@@ -4,6 +4,14 @@ function nthSmallest(arr, pos){
 
 
 
+console.log(nthSmallest([3,1,2],2),2);
+console.log(nthSmallest([15,20,7,10,4,3],3),7);
+console.log(nthSmallest([-5,-1,-6,-18],4),-1);
+console.log(nthSmallest([-102,-16,-1,-2,-367,-9],5),-2);
+console.log(nthSmallest([2,169,13,-5,0,-1],4),2);
+
+
+
 /*
 https://www.codewars.com/kata/5a512f6a80eba857280000fc/train/javascript
 
