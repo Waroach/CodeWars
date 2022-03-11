5ed60283a11160b9b907aba70744062613adbe2f
waroach <brent.rankin@gmail.com>
2022-03-11 08:16:15 -0700

    Added PSEUDOCODE 7kyu V-A-P-O-R-C-O-D-E

--- Javascript/7kyu/V-A-P-O-R-C-O-D-E.js
+++ Javascript/7kyu/V-A-P-O-R-C-O-D-E.js
@@ -5,8 +5,24 @@ vaporcode=s=>s.split(' ').join('').split('').map(l=>l=l.toUpperCase()).join("  "
 //     return string.split(' ').join('').split('').map(l=>l=l.toUpperCase()).join("  ")
 // }
 
+
+
+                            // PSEUDOCODE
+// step thru the string.
+// Take Each element and convert it to uppercase
+// Add two spaces between each letter.
+    // If white space is at the end or beginning we might have to remove that whitespace
+        // .trim() should do it.
+    // Otherwise use .join and add two whitespaces.
+        // this will not create whitespace at the end or beginning of each.
+// Return the result
+
+
+
 console.log(vaporcode("Let's go to the movies"),"L  E  T  '  S  G  O  T  O  T  H  E  M  O  V  I  E  S")
 console.log(vaporcode("Why isn't my code working?"),"W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?")
+
+
 
 /*
 https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/javascript
