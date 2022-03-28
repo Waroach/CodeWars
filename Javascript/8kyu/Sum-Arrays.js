3f9ea4bc7d977055891add498468b9d71b625720
waroach <brent.rankin@gmail.com>
2022-03-28 06:35:26 -0600

    PDEUDOCODE 8kyu Sum-Numbers

--- Javascript/8kyu/Sum-Arrays.js
+++ Javascript/8kyu/Sum-Arrays.js
@@ -2,6 +2,10 @@ function sum(numbers) {
   return numbers.reduce((a, b) => a + b, 0);
 }
 
+//PSEUDOCODE
+// Step thru the array and add each element to a total.
+// return that total
+
 /*
 https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
 
